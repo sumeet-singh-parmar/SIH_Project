76caf75836bf1f5eff2f41e3924538b174497237
Sumeet Kumar <sumeet3691@gmail.com>
2025-08-07 00:10:01 +0530

    Delete Django directory

--- Django/polls/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Tender, OEM, TENDERER, BIDDER
-
-admin.site.register(Tender)
-admin.site.register(OEM)
-admin.site.register(TENDERER)
-admin.site.register(BIDDER)
--- Django/polls/forms.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import fields
-from django.forms import ModelForm
-from .models import Tender
-
-class TenderForm(ModelForm):
-    class Meta:
-        model = Tender
-        fields = '__all__'  
